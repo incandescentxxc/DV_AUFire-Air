72a247f3e9e1bb56af102c2e160c677554770be6
incandescentxxc <incandescentxxc@gmail.com>
2020-04-10 09:56:22 +0800

    add daytime/ night time change

--- js/example.js
+++ js/example.js
@@ -1,14 +1,10 @@
-function renderGeo(){
+function start(){
     var dataFirePromise = d3.csv("http://localhost:8080/csv/fire_nrt_M6.csv");
     var mapPromise = d3.json("http://localhost:8080/aus_state.geojson");
-    Promise.all([mapPromise,dataFirePromise]).then(values => {
+    Promise.all([mapPromise, dataFirePromise]).then(values=> {
         mapData = values[0];
         fireData = values[1];
-        // Fire Data processing
-        daytimeData = fireData.filter(item => item["daynight"] === "D" && parseInt(item["confidence"]) > 60 );
-        nighttimeData = fireData.filter(item => item["daynight"] === "N" && parseInt(item["confidence"]) > 60 );
-        onedayData = daytimeData.filter(item => item["acq_date"] === "2019-10-01");
-        console.log(onedayData)
+
         //create SVG
         var width = 1000;
         var height = 700;
@@ -16,6 +12,7 @@ function renderGeo(){
             // .attr("viewBox", [0, 0, 400, 400]);
             .style("width", width)
             .style("height", height);
+        
         //projection function is a convertor which translates the cooridinates in lat and long to the actual
         //cooridinates in d3.
         var projection = d3.geoConicConformal() //projection with fine-tuned parameters exclusively for AU
@@ -34,100 +31,149 @@ function renderGeo(){
             .append('path')
             .attr('d', geoGenerator) //the attribute that defines the coordinates of a path
             .attr( "fill", "#BDC0BA" )
-            .attr( "stroke", "#999" )
+            .attr( "stroke", "#999" );
         
-        // draw data
-        min = d3.min(onedayData.map(item => item.brightness)) - 20;
-        max = d3.max(onedayData.map(item => item.brightness))
-        minSize = d3.min(onedayData.map(item => item.track * item.scan));
-        maxSize = d3.max(onedayData.map(item => item.track * item.scan));
+        // render the data for the first time
+        initData = fireData.filter(item => item["acq_date"] === "2019-10-01" && item["daynight"] === "D" && parseInt(item["confidence"]) > 60 );
+        render(svg, initData, projection);
+        renderLegend(svg);
+        var switchChecked = true;
+        d3.select("#myonoffswitch").on("change",d=>{
+            switchChecked = !switchChecked; //change into oposite
+            updateGraph(switchChecked ,fireData, projection, svg);
+        });
+        // updateGraph(circles)
+        
 
-        var colorScale = d3.scaleSequential()
+    })
+}
+
+function render(svg, data, projection){
+    console.log(data)
+    // draw data
+    // min = d3.min(data.map(item => item.brightness)) - 20;
+    // max = d3.max(data.map(item => item.brightness))
+    // minSize = d3.min(data.map(item => item.track * item.scan));
+    // maxSize = d3.max(data.map(item => item.track * item.scan));
+    var colorScale = d3.scaleSequential()
             .domain([
-                min,max
+                300,510
             ])
             .interpolator(d3.interpolateReds);
 
-        var sizeScale = d3.scaleLinear()
-            .domain([minSize, maxSize])
-            .range([4,9]);
+    var sizeScale = d3.scaleLinear()
+        .domain([1, 12])
+        .range([4,9]);
 
-        svg.append('g').selectAll("circle")
-        .data(onedayData)
-        .enter()
+    var circles = svg.selectAll("circle")
+    .data(data, d => d)
+    .join(
+        enter => enter
         .append('circle')
         .attr("cx", function(d){ return projection([d.longitude, d.latitude])[0] })
         .attr("cy", function(d){ return projection([d.longitude, d.latitude])[1] })
         .attr("r", d => sizeScale(parseInt(d.scan)*parseInt(d.track)))
         .style("fill", d => colorScale(d.brightness))
-        .attr("fill-opacity", .8);
+        .attr("fill-opacity", .8),
 
+        update => update
+        .attr("cx", function(d){ return projection([d.longitude, d.latitude])[0] })
+        .attr("cy", function(d){ return projection([d.longitude, d.latitude])[1] })
+        .attr("r", d => sizeScale(parseInt(d.scan)*parseInt(d.track)))
+        .style("fill", d => colorScale(d.brightness))
+        .attr("fill-opacity", .8),
+
+        exit => {
+            return exit.remove();
+        }
+    );
+
+}
+
+
+function renderLegend(svg){
+    const step = 0.1;
+
+    // An array interpolated over our domain where height is the height of the bar
+    const expandedDomain = d3.range(300, 510, step);
+
+    // Linear scale for y-axis
+    const yScale = d3
+    .scaleLinear()
+    .domain([300, 510])
+    .range([550, 150]);
     
-        const step = (max - min)/400
+    var colorScale = d3.scaleSequential()
+    .domain([
+        300,510
+    ])
+    .interpolator(d3.interpolateReds);
 
-        // An array interpolated over our domain where height is the height of the bar
-        const expandedDomain = d3.range(min, max, step);
+    var sizeScale = d3.scaleLinear()
+    .domain([1, 12])
+    .range([4,9]);
+    
+    // add color legend
+    svg.append('g').selectAll("rect")
+    .data(expandedDomain)
+    .enter()
+    .append('rect')
+    .attr("x",900)
+    .attr("y",d => yScale(d))
+    .attr("width", 25)
+    .attr("height",step)
+    .style("fill", d => colorScale(d))
+    .exit()
+    .remove()
 
-        // Linear scale for y-axis
-        const yScale = d3
-        .scaleLinear()
-        .domain([min, max])
-        .range([550, 150]);
-        
-        // add color legend
-        svg.append('g').selectAll("rect")
-        .data(expandedDomain)
-        .enter()
-        .append('rect')
-        .attr("x",900)
-        .attr("y",d => yScale(d))
-        .attr("width", 25)
-        .attr("height",step)
-        .style("fill", d => colorScale(d))
+    // add legend explanation
+    svg.append('text')
+    .attr("x", 900)
+    .attr("y", 120)
+    .text("Temperature in K")
+    .style("fill", "black")
+    .style("font-size", "14px")
+    .exit()
+    .remove()
 
-        // add legend explanation
-        svg.append('text')
-        .attr("x", 900)
-        .attr("y", 120)
-        .text("Temperature in K")
-        .style("fill", "black")
-        .style("font-size", "14px")
+    // add text
+    svg.append('text')
+    .attr("x", 930)
+    .attr("y", 555)
+    .text(300)
+    .style("fill", "black")
+    .style("font-size", "14px")
 
-        // add text
-        svg.append('text')
-        .attr("x", 930)
-        .attr("y", 555)
-        .text(Math.round(min))
-        .style("fill", "black")
-        .style("font-size", "14px")
+    svg.append('text')
+    .attr("x", 930)
+    .attr("y", 155)
+    .text(510)
+    .style("fill", "black")
+    .style("font-size", "14px")
 
-        svg.append('text')
-        .attr("x", 930)
-        .attr("y", 155)
-        .text(Math.round(max))
-        .style("fill", "black")
-        .style("font-size", "14px")
-
-        // add size legend
-        var legend = d3.legendSize()
-        .shape("circle")
-        .orient("vertical")
-        .scale(sizeScale)
-        .title("Fire area in k(m^2)")
-        // .color("grey")
-        
-
-        svg.append("g")
-        .attr("transform", "translate(100,100)")
-        .call(legend)
-
-
-        });
-
-        
-}
-function renderLegend(svg, colorScale, min, max){
+    // add size legend
+    var legend = d3.legendSize()
+    .shape("circle")
+    .orient("vertical")
+    .scale(sizeScale)
+    .title("Fire area in k(m^2)")
+    // .color("grey")
+    
+    svg.append("g")
+    .attr("transform", "translate(100,100)")
+    .call(legend)
 
 }
+
+function updateGraph(daynightOp, fireData, projection, svg){
+    //daynightOp: true if daytime, otherwise nighttime
+    if(daynightOp){
+        data = fireData.filter(item => item["acq_date"] === "2019-10-01" && item["daynight"] === "D" && parseInt(item["confidence"]) > 60 );
+    } else{
+        data = fireData.filter(item => item["acq_date"] === "2019-10-01" && item["daynight"] === "N" && parseInt(item["confidence"]) > 60 );
+    }
+    render(svg, data, projection);
+}
+
 
 
